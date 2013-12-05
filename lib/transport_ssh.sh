5dc79b2ca7f314795143f280558903876f3ad463
David Vossel <dvossel@redhat.com>
2013-12-05 12:28:04 -0500

    Low: add timeout value to ssh transport to avoid hanging on commands

--- lib/transport_ssh.sh
+++ lib/transport_ssh.sh
@@ -7,16 +7,18 @@ phd_ssh_cp()
 	local src=$1
 	local dest=$2
 	local node=$3
+	local fullcmd="scp -o ConnectTimeout=30 -o BatchMode=yes  $src $node:${dest}"
 
-	scp -o ConnectTimeout=30 -o BatchMode=yes  $src $node:${dest}
+	timeout -s kill 60 $fullcmd
 }
 
 phd_ssh_cmd_exec()
 {
 	local cmd=$1
 	local node=$2
+	local fullcmd="ssh -o ConnectTimeout=30 -o BatchMode=yes -l root $node $cmd"
 
-	ssh -o ConnectTimeout=30 -o BatchMode=yes -l root $node "$cmd"
+	timeout -s KILL 60 $fullcmd
 }
 
 #phd_ssh_connection_verify()
