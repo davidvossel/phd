18b2c684430f9c862889c5439aae643edd01e94f
David Vossel <dvossel@redhat.com>
2013-12-12 15:44:28 -0600

    Low: allow ssh to work with unknow hosts

--- lib/transport_ssh.sh
+++ lib/transport_ssh.sh
@@ -1,13 +1,14 @@
 #!/bin/bash
 
 #. ${PHDCONST_ROOT}/lib/phd_utils_api.sh
+PHD_SSH_OPTS="-o StrictHostKeyChecking=no -o ConnectTimeout=30 -o BatchMode=yes"
 
 phd_ssh_cp()
 {
 	local src=$1
 	local dest=$2
 	local node=$3
-	local fullcmd="scp -o ConnectTimeout=30 -o BatchMode=yes  $src $node:${dest}"
+	local fullcmd="scp $PHD_SSH_OPTS $src $node:${dest}"
 
 	timeout -s kill 120 $fullcmd
 }
@@ -16,7 +17,7 @@ phd_ssh_cmd_exec()
 {
 	local cmd=$1
 	local node=$2
-	local fullcmd="ssh -o ConnectTimeout=30 -o BatchMode=yes -l root $node $cmd"
+	local fullcmd="ssh $PHD_SSH_OPTS -l root $node $cmd"
 
 	timeout -s KILL 120 $fullcmd
 }
