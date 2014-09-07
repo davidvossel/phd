bfeb7ce9479f21047d50d7e8a2dc626d3dc8131a
David Vossel <dvossel@redhat.com>
2014-09-07 10:01:58 -0500

    Low: misc: remove requirement for quotes for docker cmds with more than one arg

--- misc/docker_cmd.sh
+++ misc/docker_cmd.sh
@@ -4,4 +4,7 @@ if [ -z "$2" ]; then
 	echo 'usage - ./docker_cmd <docker container id> <cmd>'
 fi 
 
-echo "$2" | nsenter --target $(docker inspect --format {{.State.Pid}} ${1}) --mount --uts --ipc --net --pid
+host=$1
+shift;
+
+echo "$@" | nsenter --target $(docker inspect --format {{.State.Pid}} ${host}) --mount --uts --ipc --net --pid
