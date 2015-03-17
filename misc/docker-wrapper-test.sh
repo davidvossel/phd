6014e60d6c4ba21a4a52bc94a8faacb768f1215c
David Vossel <dvossel@redhat.com>
2015-03-17 18:14:23 -0400

    low: misc: more docker-wrapper tests

--- misc/docker-wrapper-test.sh
+++ misc/docker-wrapper-test.sh
@@ -60,6 +60,7 @@ docker_exec()
 	local expected_rc=$2
 	local rc
 
+	echo "---- Executing $cmd ----"
 	/usr/libexec/pacemaker/container_wrappers/docker-wrapper $cmd
 	rc=$?	
 
@@ -82,9 +83,9 @@ test_simple()
 	docker_exec "monitor" "7"
 	docker_exec "start" "0"
 	docker_exec "monitor" "0"
-	docker_exec "monitor" "0"
 	docker_exec "start" "0"
 	docker_exec "monitor" "0"
+	docker_exec "stop" "0"
 	docker_exec "stop" "0"
 	docker_exec "monitor" "7"
 }
@@ -108,6 +109,91 @@ test_failure_detection()
 	docker_exec "stop" "0"
 }
 
+
+test_multi_rsc()
+{
+	clear_vars
+	curtest="multi_rsc"
+
+	export OCF_RESKEY_CRM_meta_provider="heartbeat" OCF_RESKEY_CRM_meta_class="ocf" OCF_RESKEY_CRM_meta_type="Dummy" OCF_RESKEY_CRM_wrapper_instance="$container"
+	export OCF_RESOURCE_INSTANCE="test" OCF_RESKEY_docker_image="$image"  OCF_RESKEY_docker_privileged="true"
+
+	docker_exec "monitor" "7"
+	docker_exec "start" "0"
+	docker_exec "monitor" "0"
+
+	export OCF_RESOURCE_INSTANCE="test2"
+	docker_exec "monitor" "7"
+	docker_exec "start" "0"
+	docker_exec "monitor" "0"
+
+
+	docker_exec "stop" "0"
+	val=$(docker inspect --format {{.State.Running}} $container 2>/dev/null)
+	if [ $? -ne 0 ]; then
+		#not running as a result of container not being found
+		echo "FAILED: test $curtest: container shouldn't have stopped"
+		exit 1
+	fi
+
+	export OCF_RESOURCE_INSTANCE="test"
+	docker_exec "monitor" "0"
+	docker_exec "stop" "0"
+
+	val=$(docker inspect --format {{.State.Running}} $container 2>/dev/null)
+	if [ $? -eq 0 ]; then
+		#not running as a result of container not being found
+		echo "FAILED: test $curtest: container should be stopped now"
+		exit 1
+	fi
+}
+
+
+test_super_multi_rsc()
+{
+	clear_vars
+	curtest="super_multi_rsc"
+	resources=5
+
+	export OCF_RESKEY_CRM_meta_provider="heartbeat" OCF_RESKEY_CRM_meta_class="ocf" OCF_RESKEY_CRM_meta_type="Dummy" OCF_RESKEY_CRM_wrapper_instance="$container"
+	export OCF_RESOURCE_INSTANCE="test" OCF_RESKEY_docker_image="$image"  OCF_RESKEY_docker_privileged="true"
+
+	for (( c=1; c <= $resources; c++ ))
+	do
+		export OCF_RESOURCE_INSTANCE="test${c}"
+
+		docker_exec "monitor" "7"
+		docker_exec "start" "0"
+		docker_exec "monitor" "0"
+
+	done
+
+	for (( c=1; c <= $resources; c++ ))
+	do
+		export OCF_RESOURCE_INSTANCE="test${c}"
+		docker_exec "monitor" "0"
+	done
+
+	for (( c=1; c <= $resources; c++ ))
+	do
+		export OCF_RESOURCE_INSTANCE="test${c}"
+		docker_exec "stop" "0"
+
+		val=$(docker inspect --format {{.State.Running}} $container 2>/dev/null)
+		rc=$?
+		if [ $rc -ne 0 ] && [ $c -ne $resources ]; then
+			echo "FAILED: test $curtest: container shouldn't have stopped yet. resource $OCF_RESOURCE_INSTANCE stopped last"
+			exit 1
+		elif [ $rc -eq 0 ] && [ $c -eq $resources ]; then
+			echo "FAILED: test $curtest: container should be stopped now"
+			exit 1
+		fi
+		docker_exec "monitor" "7"
+	done
+
+}
+
+
 service docker start > /dev/null 2>&1
 build_image
 
@@ -117,6 +203,12 @@ test_simple
 echo "--------------- PASSED: $curtest"
 
 test_failure_detection
+echo "--------------- PASSED: $curtest"
+
+test_multi_rsc
+echo "--------------- PASSED: $curtest"
+
+test_super_multi_rsc
 echo "--------------- PASSED: $curtest"
 
 cleanup
